554088dcfd5408629304690bb0ece560f9659c64
jenbirch10 <jenbirch10@gmail.com>
2015-10-24 10:32:26 -0500

    -include jquery, knockout.js -ajax request for json data -parse json data

---
+++ js/viewmodel.js
@@ -0,0 +1,28 @@
+
+
+//load neighborhood data
+var data = [];
+var viewModel = {
+	places: ko.observableArray(data)
+};
+$(document).ready(function() {
+	ko.applyBindings(viewModel);
+});
+
+$.getJSON("js/neighborhood.json", function(data) { 
+	viewModel.places(data);
+	console.log(data);
+}).fail(function() {console.log('error')})
+
+
+
+// function initialize() {
+// 			var mapCanvas = document.getElementById('map');
+//        		var mapOptions = {
+//          		center: new google.maps.LatLng(41.924707, -87.700333),
+//           		zoom: 15,
+//           		mapTypeId: google.maps.MapTypeId.ROADMAP
+//         		}
+//         	var map = new google.maps.Map(mapCanvas, mapOptions)
+//       	}
+//       	google.maps.event.addDomListener(window, 'load', initialize);
