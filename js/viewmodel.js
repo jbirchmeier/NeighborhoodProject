9e732dccb3f4504c01ea2cbef21617da40ec6a6c
jenbirch10 <jenbirch10@gmail.com>
2015-10-25 12:15:27 -0500

    Add places markers
    add markers for each JSON object - need to troubleshoot marker
    animation error(s) “set/getAnimation not a function”

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,6 +1,6 @@
 
 
-//load neighborhood data
+//create empty data array, apply Knockout.js
 var data = [];
 var viewModel = {
 	places: ko.observableArray(data)
@@ -9,20 +9,50 @@ $(document).ready(function() {
 	ko.applyBindings(viewModel);
 });
 
+var myPlaces = new Array();
+
+//load JSON neighborhood data from neighborhood.json
 $.getJSON("js/neighborhood.json", function(data) { 
 	viewModel.places(data);
+	$.each(data, function(i) {
+		myPlaces.push(data[i]);
+	});
 	console.log(data);
 }).fail(function() {console.log('error')})
 
+var marker = [];
+var content = [];
+var infowindow = [];
 
-
+//create and set Google Map with markers
 function initialize() {
-			var mapCanvas = document.getElementById('map');
-       		var mapOptions = {
-         		center: new google.maps.LatLng(41.924707, -87.700333),
-          		zoom: 15,
-          		mapTypeId: google.maps.MapTypeId.ROADMAP
-        		}
-        	var map = new google.maps.Map(mapCanvas, mapOptions)
-      	}
-      	google.maps.event.addDomListener(window, 'load', initialize);
+	var mapCanvas = document.getElementById('map');
+		var mapOptions = {
+ 		center: new google.maps.LatLng(41.924707, -87.700333),
+  		zoom: 14,
+  		mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+	var map = new google.maps.Map(mapCanvas, mapOptions)
+
+	for (i=0; i<myPlaces.length; i++) {
+		marker[i]= new google.maps.Marker({
+			position: {lat: myPlaces[i].lat, lng:myPlaces[i].long},
+			map: map,
+			title: myPlaces[i].name,
+			animation: google.maps.Animation.DROP
+		});
+		marker[i].addListener('click', toggleBounce);
+	}
+
+//bounce markers on click - need to fix!
+	function toggleBounce() {
+		if(marker.getAnimation() !== null) {
+			console.log('isnull');
+			marker.setAnimation(null);
+		} else {
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+		}
+	}
+}
+//run initialize function for google map on window load
+google.maps.event.addDomListener(window, 'load', initialize);
