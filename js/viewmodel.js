2e97bae15f9366a0cd9f525884fab49214e4e5e5
jenbirch10 <jenbirch10@gmail.com>
2015-11-04 07:31:36 -0600

    -added info window and window content

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -22,7 +22,6 @@ var viewModel = {
 	},
 	search: function(value) {
 		viewModel.myPlaces.removeAll();
-
 		for(var x in myPlaces) {
 			if(myPlaces[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0){
 				viewModel.myPlaces.push(myPlaces[x]);
@@ -37,11 +36,12 @@ $(document).ready(function() {
 viewModel.query.subscribe(viewModel.search);
 
 
+
 var marker = [];
 var content = [];
 var infowindow = [];
 
-//create and set Google Map with markers
+//create and set Google Map with marker
 function initialize() {
 	var mapCanvas = document.getElementById('map');
 		var mapOptions = {
@@ -53,24 +53,38 @@ function initialize() {
 
 //loop through myPlaces array and place a marker at each lat/long
 	for (i=0; i<myPlaces.length; i++) {
-		marker[i]= new google.maps.Marker({
+		marker = marker[i]
+		marker = new google.maps.Marker({
 			position: {lat: myPlaces[i].lat, lng:myPlaces[i].long},
 			map: map,
 			title: myPlaces[i].name,
 			animation: google.maps.Animation.DROP
 		});
-	google.maps.event.addListener(marker, 'click', toggleBounce);	
-	}
-	
-//bounce markers on click - need to fix!
-	function toggleBounce() {
-		if(marker.getAnimation() !== null) {
-			console.log('isnull');
-			marker.setAnimation(null);
+		google.maps.event.addListener(marker, 'click', toggleBounce);
+		google.maps.event.addListener(marker, 'click', function() {
+    	infowindow.open(map, this);
+  		}); //end listener
+
+//where does infowindow go?!
+		var infowindow = new google.maps.InfoWindow({
+    	content: '<div>'+ myPlaces[i].type+ '</div>' + '<div>'+ myPlaces[i].summary+ '</div>' + '<div>'+ myPlaces[i].url+ '</div>' 
+  		});
+
+	} //end for loop
+
+
+
+
+//bounce markers on click 
+	function toggleBounce(marker) {
+		if(this.getAnimation() !== null) {
+			this.setAnimation(null);
 		} else {
-			marker.setAnimation(google.maps.Animation.BOUNCE);
+			this.setAnimation(google.maps.Animation.BOUNCE);
 		}
 	}
+
+
 }
 //run initialize function for google map on window load
 google.maps.event.addDomListener(window, 'load', initialize);
